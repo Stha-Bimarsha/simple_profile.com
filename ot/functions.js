9aae576d64f138be7acdc27db4e3f6a630c360b2
Aman-Ido <110373355+Aman-Ido@users.noreply.github.com>
2022-08-02 15:18:17 +0545

    Add files via upload

--- ot/functions.js
+++ ot/functions.js
@@ -1,13 +1,19 @@
 const glow_this = document.querySelectorAll(".glow_this");
 let tracker = 0;
 
-glow_this.forEach(function (e) {
-	setInterval (function () {
-		if (tracker == 1) {
-			e.style.color = "grey";
-			tracker = 0;
-		}
-		console.log("The value of tracker is " + tracker);
-	}, 5000);
-	
+glow_this.forEach(function (value_this) {
+	value_this.style.color = "white";
 })
+
+// array of pictures
+const back_img = ["one", "two", "three", "four"];
+// selecting the div with picture background 
+const second_container = document.getElementById("second_container");
+let i = 1;
+setInterval(function () {
+	if (i >= back_img.length) {
+		i = 0;
+	}
+	second_container.style.backgroundImage = `url("./img/${back_img[i]}.jpg")`;
+	i++;
+}, 2500);
